f1f723975ca05850192e01168027c65ed23d30a2
ifeanyi <dennis@ifeanyi.io>
2017-02-15 13:39:08 +0000

    install elasticsearch kibana nginx

--- provision.sh
+++ provision.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-#install java jdk and jre
+#provisioning for ubuntu 14.X
+# INSTALLL JAVA AND JRE
 apt-get -y install default-jdk
 
-#install elasticsearch version 5.X
+# INSTALL ELASTICSEARCH 5.X
 echo "Installing Elasticsearch"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 apt-get -y update
 apt-get -y install elasticsearch
 sed -i 's/#network.host: 192.168.0.1/network.host: localhost/g' /etc/elasticsearch/elasticsearch.yml
-sed -i 's/-Xms2g/-Xms512m/g' /etc/elasticsearch/jvm.opt
+sed -i 's/-Xms2g/-Xms512m/g' /etc/elasticsearch/jvm.options
 sed -i 's/-Xmx2g/-Xmx512m/g' /etc/elasticsearch/jvm.options
 systemctl enable elasticsearch.service
 systemctl start elasticsearch.service
 
-#install kibana version 5.X
-apt-get -y install kibana
-systemctl enable kibana
-systemctl start kibana
+# INSTALL KIBANA 5.X kibana version 5.X
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+apt-get update && sudo apt-get -y install kibana
+sed -i 's/#service.host: 0.0.0.0/service.host: localhost/g' /etc/kibana/kibana.yml
+systemctl enable kibana.service
+systemctl start kibana.service
 
-#install nginx
-apt-get -y install nginx
-systemctl enable kibana
+# INSTALL NGINX
+apt-get -y install nginx apache2-utils
+#create kibana user "kibanaadmin"
+htpasswd -c /etc/nginx/htpasswd.users kibanaadmin
+#replace /etc/nginx/sites-available/default WITH nginxproxy
+mv nginxproxy /etc/nginx/sites-available/default
 systemctl restart nginx.service
+
+
+# INSTALL LOGSTASH
+#echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo apt-get update && sudo apt-get install kibana
